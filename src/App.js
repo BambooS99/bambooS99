de47aaadc4cbf45830aed4f629b0ca4a664ff1cb
BambooS99 <owaishassan786@gmail.com>
2025-05-12 00:26:16 -0400

    made changes to the react Repo and this is the actual first commit. ignore the other commits

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className = 'App-heading-text'>
+          <h1>Translation App</h1>
+         
+         <div className='app-body'>
+          enter the link here  <input></input> <button>Go</button>
+          </div>
+        </div>
+       
+        
       </header>
     </div>
   );
